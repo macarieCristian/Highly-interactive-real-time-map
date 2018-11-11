23ebba984db9cc5416846ebf1e69bdc88c271574
Macarie Cristian <33750050+macarieCristian@users.noreply.github.com>
2018-11-12 00:47:24 +0200

    cleanup

--- Template/persistance/src/main/java/ro/licence/cristian/persistence/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package ro.licence.cristian.persistence.repository;
-
-import ro.licence.cristian.persistence.model.AppUser;
-
-public interface UserRepository extends TemplateRepository<AppUser, Long> {
-}
